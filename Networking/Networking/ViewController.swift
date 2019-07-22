0d081d8434cf515bb35539c0a89dc63917c60f23
Viacheslav Bilyi <>
2019-07-22 19:53:56 +0300

    added project;

---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Viacheslav Bilyi on 7/22/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
